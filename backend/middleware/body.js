fa6a53b7bf9d34635203c0b26f86be983b5d9245
otissv <otissv@gmail.com>
2015-06-01 12:01:01 +0100

    added body parser

---
+++ backend/middleware/body.js
@@ -0,0 +1,15 @@
+/*
+* Body parser
+* Should be placed before express.static
+*/
+
+'use strict';
+
+import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
+
+module.exports = function(app) {
+  app.use(bodyParser.json()); // Parse application/json
+  app.use(bodyParser.urlencoded({ extended: false })); // Parse application/x-www-form-urlencoded
+  app.use(methodOverride());// Over ride request header
+};
