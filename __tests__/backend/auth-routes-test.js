b43e8ae58ce368874b55994ca3a679996be23a88
otissv <otissv@gmail.com>
2015-06-03 11:22:15 +0100

    added pending tests

--- __tests__/backend/auth-routes-test.js
+++ __tests__/backend/auth-routes-test.js
@@ -6,6 +6,7 @@ import helpers from '../test-helpers.js';
 
 
 describe('Authorisation routes', () => {
+
   before( () => {
     helpers.collections({
       url   : 'mongodb://127.0.0.1:27017/test',
@@ -14,46 +15,66 @@ describe('Authorisation routes', () => {
     });
   });
 
+
   after( () => {
     helpers.uncache('../../backend/app.js');
   });
 
-  it('Can get /signup', done => {
-    request(app)
-      .get('/')
-      .expect(200, done);
+
+  describe('Signup routes', () => {
+
+    it('GET /signup - User can get sign up page', done => {
+      request(app)
+        .get('/')
+        .expect(200, done);
+    });
+
+
+    it('POST /signup - User cannot sign up with missing credentials', done => {
+      request(app)
+        .post('/signup')
+        .expect(401, done);
+    });
+
+
+    it('POST /signup - User can sign up', done => {
+      let signupDetails = {
+        username: 'otis',
+        password: 'xyz'
+      };
+
+      request(app)
+        .post('/signup')
+        .send(signupDetails)
+        .expect(302, done);
+    });
+
+
+    it('POST /signup - User already exists', done => {
+      let signupDetails = {
+        username: 'otis',
+        password: 'xyz'
+      };
+
+      request(app)
+        .post('/signup')
+        .send(signupDetails)
+        .expect(401, done);
+    });
   });
 
-  it('Unauthorized post to /signup', done => {
-    request(app)
-      .post('/signup')
-      .expect(401, done);
+
+  describe('Signin routes', () => {
+
+    it('GET  /signin - User can get sign in page');
+    it('POST /signin - User cannot sign in with missing credentials');
+    it('POST /signin - User cannot sign in with incorrect credentials');
+    it('POST /signin - User can sign in');
   });
 
-  it('can post to /signup', done => {
-    let signupDetails = {
-      username: 'otis',
-      password: 'xyz'
-    };
 
-    request(app)
-      .post('/signup')
-      .send(signupDetails)
-      .expect(302, done);
+  describe('Signout route', () => {
+
+    it('POST /signout - User can sign out & redirected to /');
   });
-
-  it('User already exists', done => {
-    let signupDetails = {
-      username: 'otis',
-      password: 'xyz'
-    };
-
-    request(app)
-      .post('/signup')
-      .send(signupDetails)
-      .expect(401, done);
-  });
-
-  it('User already exists');
-
 });
